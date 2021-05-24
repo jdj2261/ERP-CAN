8c40a58e76b0c9a0726257446da0f42061a0572a
jdj2261 <wlseowhd12@naver.com>
2021-05-24 15:08:12 +0900

    deleted using-python

--- using-python/can/interfaces/ics_neovi/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-"""
-"""
-
-from can.interfaces.ics_neovi.neovi_bus import NeoViBus
--- using-python/can/interfaces/pcan/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-"""
-"""
-
-from can.interfaces.pcan.pcan import PcanBus
